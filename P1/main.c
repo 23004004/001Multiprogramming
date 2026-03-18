44cba1ffb91984b759ca6dc80977b75d2293b9f5
Gabriel Garcia <gabriel.garcia@galileo.edu>
2026-03-18 10:57:43 -0600

    Added small delay to prevent overwhelming UART

--- P1/main.c
+++ P1/main.c
@@ -1,14 +1,21 @@
 #include "../lib/stdio.h"
 
-void main(void) {
+void main(void)
+{
     int i = 0;
 
-    while (1) {
-        PRINT("%s %d\n", "----From P1:", i);
+    while (1)
+    {
+        PRINT("----From P1: %d\n", i);
 
         i++;
-        if (i == 10) {
+        if (i == 10)
+        {
             i = 0;
         }
+
+        // Small delay to prevent overwhelming UART
+        for (volatile int i = 0; i < 150000000; i++)
+            ;
     }
 }
--- P2/main.c
+++ P2/main.c
@@ -1,16 +1,21 @@
 #include "../lib/stdio.h"
 
-void main(void){
-
+void main(void)
+{
     char i = 'a';
 
-    while (1) {
+    while (1)
+    {
         PRINT("----From P2: %c\n", i);
 
         i++;
-        if (i > 'z') {
+        if (i > 'z')
+        {
             i = 'a';
         }
+
+        // Small delay to prevent overwhelming UART
+        for (volatile int i = 0; i < 150000000; i++)
+            ;
     }
-    
 }
